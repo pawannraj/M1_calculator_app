a2b423ab7f3020f0db924121457de2189c4bdcff
pawannraj <98880956+pawannraj@users.noreply.github.com>
2022-03-03 11:06:55 +0530

    Update main.c

--- 3_implementation/main.c
+++ 3_implementation/main.c
@@ -1,177 +1,96 @@
-#include<stdio.h>
-#include<math.h>
+#include"calculator.h"
 
+ int addition();
+int subtraction();
+int multiplication();
+int division();
+int modulus();
+int power();
+int factorial();
+int cube(); // it is header file, conatins all function
+int main() {
+  // variable declaration
+  int choice; 
+  int n1,n2, ans=0;
+  // double s ;
+  do
+  {
+  // printf("Select the operation you want perform");
+  // printf("\n");
+  // printf("\n1.Additon\n 2.Subtraction\n 3.multplication \n 4.Division \n 5.Modules\n 6.Power\n 7.Factorial \n 8.Cube \n 9.Exit \n  ");
+  //  printf("\n");
+  // scanf("%d", &choice);
+choice=Menu();
+// switch statement
+ switch (choice) {
 
-int main(void) {
-int choice, i, a, b;
-float x, y, result;
-do {
-printf("\nSelect your operation (0 to exit):\n");
-printf("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n");
-printf("5. Square root\n6. X ^ Y\n7. X ^ 2\n8. X ^ 3\n");
-printf("9. 1 / X\n10. X ^ (1 / Y)\n11. X ^ (1 / 3)\n");
-printf("12. 10 ^ X\n13. X!\n14. %% \n15. log10(x)\n16. Modulus\n");
-printf("17. Sin(X)\n18. Cos(X)\n19. Tan(X)\n20. Cosec(X)\n");
-printf("21. Cot(X)\n22. Sec(X)\n");
-printf("Choice: ");
-scanf("%d", &choice);
-if(choice == 0) return 0;
-switch(choice) {
-case 1:
-printf("Enter X: ");
-scanf("%f", &x);
-printf("\nEnter Y: ");
-scanf("%f", &y);
-result = x + y;
-printf("\nResult: %f", result);
-break;
-case 2:
-printf("Enter X: ");
-scanf("%f", &x);
-printf("\nEnter Y: ");
-scanf("%f", &y);
-result = x-y;
-printf("\nResult: %f", result);
-break;
-case 3:
-printf("Enter X: ");
-scanf("%f", &x);
-printf("\nEnter Y: ");
-scanf("%f", &y);
-result = x * y;
-printf("\nResult: %f", result);
-break;
-case 4:
-printf("Enter X: ");
-scanf("%f", &x);
-printf("\nEnter Y: ");
-scanf("%f", &y);
-result = x / y;
-printf("\nResult: %f", result);
-break;
-case 5:
-printf("Enter X: ");
-scanf("%f", &x);
-result = sqrt(x);
-printf("\nResult: %f", result);
-break;
-case 6:
-printf("Enter X: ");
-scanf("%f", &x);
-printf("\nEnter Y: ");
-scanf("%f", &y);
-result = pow(x, y);
-printf("\nResult: %f", result);
-break;
-case 7:
-printf("Enter X: ");
-scanf("%f", &x);
-result = pow(x, 2);
-printf("\nResult: %f", result);
-break;
-case 8:
-printf("Enter X: ");
-scanf("%f", &x);
-result = pow(x, 3);
-printf("\nResult: %f", result);
-break;
-case 9:
-printf("Enter X: ");
-scanf("%f", &x);
-result = pow(x, -1);
-printf("\nResult: %f", result);
-break;
-case 10:
-printf("Enter X: ");
-scanf("%f", &x);
-printf("\nEnter Y: ");
-scanf("%f", &y);
-result = pow(x, (1/y));
-printf("\nResult: %f", result);
-break;
-case 11:
-printf("Enter X: ");
-scanf("%f", &x);
-y = 3;
-result = pow(x, (1/y));
-printf("\nResult: %f", result);
-break;
-case 12:
-printf("Enter X: ");
-scanf("%f", &x);
-result = pow(10, x);
-printf("\nResult: %f", result);
-break;
-case 13:
-printf("Enter X: ");
-scanf("%f", &x);
-result = 1;
-for(i = 1; i <= x; i++) {
-result = result * i;
-}
-printf("\nResult: %.f", result);
-break;
-case 14:
-printf("Enter X: ");
-scanf("%f", &x);
-printf("\nEnter Y: ");
-scanf("%f", &y);
-result = (x * y) / 100;
-printf("\nResult: %.2f", result);
-break;
-case 15:
-printf("Enter X: ");
-scanf("%f", &x);
-result = log10(x);
-printf("\nResult: %.2f", result);
-break;
-case 16:
-printf("Enter X: ");
-scanf("%d", &a);
-printf("\nEnter Y: ");
-scanf("%d", &b);
-result = a % b;
-printf("\nResult: %d", (int)result);
-break;
-case 17:
-printf("Enter X: ");
-scanf("%f", &x);
-result = sin(x * 3.14159 / 180);
-printf("\nResult: %.2f", result);
-break;
-case 18:
-printf("Enter X: ");
-scanf("%f", &x);
-result = cos(x * 3.14159 / 180);
-printf("\nResult: %.2f", result);
-break;
-case 19:
-printf("Enter X: ");
-scanf("%f", &x);
-result = tan(x * 3.14159 / 180);
-printf("\nResult: %.2f", result);
-break;
-case 20:
-printf("Enter X: ");
-scanf("%f", &x);
-result = 1 / (sin(x * 3.14159 / 180));
-printf("\nResult: %.2f", result);
-break;
-case 21:
-printf("Enter X: ");
-scanf("%f", &x);
-result = 1 / tan(x * 3.14159 / 180);
-printf("\nResult: %.2f", result);
-break;
-case 22:
-printf("Enter X: ");
-scanf("%f", &x);
-result = 1 / cos(x * 3.14159 / 180);
-printf("\nResult: %.2f", result);
-break;
-default:
-printf("\nInvalid Choice!");
-}
-} while(choice);
-return 0;
+    case 1:
+      printf("Enter two number \n");
+      scanf("%d %d",&n1,&n2);
+      ans = addition(n1,n2); //calling addition function
+      
+      printf("Addition of two numbers is  %d \n",ans);
+      break;
+    case 2:
+     printf("Enter two number \n1");
+      scanf("%d %d",&n1,&n2);
+      ans=subtraction(n1,n2); //calling subtraction function
+      printf("subtraction of two numbers is  %d \n",ans);
+      break;
+    case 3:
+      printf("Enter two number \n");
+      scanf("%d %d",&n1,&n2);
+      ans = multiplication(n1,n2); //calling multiply function 
+      printf("multiplication of two numbers is  %d \n",ans);
+      break;
+    case 4:
+      printf("Enter two number \n");
+      scanf("%d %d",&n1,&n2);
+      if(n2==0)
+    {
+     printf (" \n Denominator cannot be zero. Please enter another value ");  
+        scanf ("%d", &n2);
+        ans = division(n1,n2);
+    }
+     ans = division(n1,n2);    //calling division function
+     printf("division of two numbers is  %d \n",ans);
+
+      break;
+    case 5: 
+        printf("Enter two number \n");
+        scanf("%d %d",&n1,&n2);
+       ans= modulus(n1,n2);  // calling madules function
+       printf("Modulus of these two numbers is  %d \n",ans);
+        break;  
+    case 6: 
+        printf("\nEnter number to find the power \n");
+        printf("number: ");
+        scanf("%d",&n1);
+       ans= power(n1);
+          printf("Power of %d is = %d \n",n1,ans);
+        break;  
+    case 7: 
+        printf("\nEnter a number to find factorial : \n");
+        scanf("%d",&n1);
+        ans = factorial(n1); //calling  factorial function
+        printf("factorial of %d is  %d \n",n1,ans);
+        break;  
+        case 8: 
+        printf("\nEnter number to find the cube \n");
+        printf("number: ");
+        scanf("%d",&n1);
+       ans= cube(n1);
+          printf("Cube of %d is  %d \n",n1,ans);
+        break; 
+         case 9:  
+            exit(0); // It call the exit() function  to exit from the program  
+            break; // break the function  
+    // operator doesn't match any case constant
+    default:
+      printf("Error! operator is not correct");
+  }
+   printf (" \n \n ------------------------------\n ");  
+    } while (choice != 9);  
+   
+  return 0;
 }
