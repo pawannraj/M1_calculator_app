eb1bc9bc39f01d5192e5ea426126d48a669c67fd
pawannraj <98880956+pawannraj@users.noreply.github.com>
2022-03-04 13:24:43 +0530

    Update test.c

--- 3_implementation/Test/test.c
+++ 3_implementation/Test/test.c
@@ -9,16 +9,16 @@ void tearDown()
 {
 }
 
-void test_add()
+void test_sum()
 {
     int n1;
     int n2;
     int actual_result;
-    TEST_ASSERT_EQUAL(add(n1,n2),actual_result);
+    TEST_ASSERT_EQUAL(sum(n1,n2),actual_result);
 }
 int test_main()
 {
     UNITY_BEGIN();
-     RUN_TEST(test_add);
+     RUN_TEST(test_sum);
      return UNITY_END();
 }
